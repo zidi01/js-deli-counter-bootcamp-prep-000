6f491507b55d09b2a8b8481f2e383e81542c1315
zidi <zidi01@ukr.net>
2018-07-19 17:54:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -45,3 +45,18 @@ function currentLine (array, item) {
 }
     
 currentLine(currLineCopy);
+
+
+function currentLine(line) {
+  if (!line.length) {
+    return "The line is currently empty."
+  }
+
+  const numbersAndNames = []
+
+  for (let i = 0, l = line.length; i < l; i++) {
+    numbersAndNames.push(`${i + 1}. ${line[i]}`)
+  }
+
+  return `The line is currently: ${numbersAndNames.join(', ')}`
+};
